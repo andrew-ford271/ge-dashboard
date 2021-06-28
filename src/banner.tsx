4a5a55a18bfbabaec81edec4180910e9d4c29d68
alex <alxford45@gmail.com>
2021-06-27 21:24:10 -0500

    migrated files from jsx to tsx

--- src/banner.tsx
+++ src/banner.tsx
@@ -1,15 +1,15 @@
-import TextField from "@material-ui/core/TextField";
-import Autocomplete from "@material-ui/lab/Autocomplete";
-import React from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,12 +27,12 @@ export function Banner(props) {
   const { setValue } = props;
   const [state, setState] = React.useState({
     loading: true,
-    data: [],
+    data: [] as any[],
   }); // state initialized to loading
   React.useEffect(() => {
     const getData = async () => {
       const d = await axios.get(
-        "https://prices.runescape.wiki/api/v1/osrs/mapping"
+        'https://prices.runescape.wiki/api/v1/osrs/mapping'
       );
       let items = new Array();
       for (let i = 0; i < d.data.length; i++) {
@@ -59,14 +59,13 @@ export function Banner(props) {
             edge="start"
             className={classes.menuButton}
             color="inherit"
-            aria-label="menu"
-          >
+            aria-label="menu">
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             Price Checker
           </Typography>
-          <div style={{ backgroundColor: "white" }}>
+          <div style={{ backgroundColor: 'white' }}>
             <Autocomplete
               id="search-bar"
               options={state.data}
